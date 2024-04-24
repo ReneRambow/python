f44818a3f3805497ba7cf77ef13da4f9ab66c630
ReneRambow <bill12gate@gmail.com>
2024-04-24 14:12:56 +0200

    les1.py

---
+++ les.py
@@ -0,0 +1,11 @@
+print("Hallo Wereld")
+naam = input("Hoe heet je?")
+print(f'Welkom {naam}')
+
+studenten = int(input("Hoveel studenten zijn er?"))
+lesgeld = 1357
+
+totaal = studenten*lesgeld
+print(f'Het aangegeven studenten is: {studenten}')
+print(f'Het huidige lesgeld per jaar is €{lesgeld},-')
+print(f'Het totale lesgeld per jaar voor de studenten is €{totaal},-')
