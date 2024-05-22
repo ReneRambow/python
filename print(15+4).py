1d519928e20142540d71ab7ec5d8b9dcb960287f
ReneRambow <bill12gate@gmail.com>
2024-05-22 14:47:25 +0200

    test

---
+++ print(15+4).py
@@ -0,0 +1,56 @@
+##opdracht 1
+print( 15+4 ) #addition
+print( 15-4 ) #substraction
+print( 15*4 ) #multiplication
+print( 15/4 ) #division
+print( 15//4 ) #floor division
+print( 15**4 ) #exponentiation
+print( 15%4 ) #modulus
+
+##opdracht 2
+print( 5*2 -3+4/2 )
+print( 5*2 - 3+4 / 2 ) 
+##9.0 zelfde als erboven
+
+print( (5*2) - (3+4) /2 )
+print( ((5*2) -(3+4)) / 2 ) 
+##de reken volgorde is eerst () daarna * en / als laatst + en -
+
+##opdracht 3
+naam = "Rene"
+print ("Hallo " + naam + ",ik leer nu programeren") 
+##addition operator, naam verandering werkt
+
+##opdracht 4
+#naam1 = 252
+#print ("Hallo " + naam1 + ",ik leer nu programeren") 
+##werkt niet in verband met dat het nu een interger is
+
+##opdracht 5
+#getal1 = 25
+#getal2 = 0
+#print(getal1 / getal2)
+##foutmelding in verband met dat je door 0 probeert te delen
+
+#opdracht 6
+#print( (2*3) /4 + (5 -(6/7)) *8 ) # We verwachten: 34.642857142857146, print( ((2*3 /4 + (5 -6/7) *8 ) aangepaste staat links
+#print( ((12*13 /14) + (15 -16) /17) *18 ) # We verwachten: 199.5126050420168, niks hoeven aan te passen
+#
+##opdracht 7
+#print( (431 / 100) * 100 ) #hij geeft een getal met erg veel decimalen
+#
+##opdracht 8
+#print( 625//13 ) #floor division
+#print( 625%13 ) #modulus
+
+#opdracht 9
+
+
+
+
+
+
+
+
+#for x in range(1, 1001):
+    #print(f'{x}e keer: Lekker bezig ')
